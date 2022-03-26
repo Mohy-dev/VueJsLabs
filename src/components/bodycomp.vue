310803e56e48d9380fb9437894b2e501c029de6d
mohy-dev <mohy.dev@gmail.com>
2022-03-26 10:44:21 +0200

    lab2.2

--- src/components/bodycomp.vue
+++ src/components/bodycomp.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="container">
- <form v-if="showMain" class="row g-3 needs-validation" novalidate>
+ <form v-if="showMain" @submit="checkForm" class="row g-3 needs-validation" novalidate>
+   <div>
   <div class="col-md-4 position-relative">
     <label for="validationTooltip01" class="form-label">First name</label>
     <input v-model="fname" type="text" class="form-control" id="validationTooltip01" required>
@@ -30,8 +31,11 @@
     <div class="invalid-tooltip">
         Fill it please
     </div>
+    <div v-for="error in errors" :key="error">
+      {{error}}
+    </div>
   </div>
-  <div class="col-md-12 position-relative">
+  <div class="col-md-4 position-relative">
     <label for="validationTooltip01" class="form-label">Email</label>
     <input v-model="email" type="email" class="form-control" id="validationTooltip01" required>
     <div class="valid-tooltip">
@@ -41,15 +45,24 @@
         Fill it please
     </div>
   </div>
-  <div class="col-12">  
+  <div class="col-12 mt-3">  
     <button class="btn btn-primary col-12" type="submit" @click="showData=true, showMain=false">Confirm Info</button>
   </div>
+  </div>
 </form>
-<div v-if="showData">
-  <label for="nameInfo">Your Name</label>
-  <!-- <P id="nameInfo" v-model="fname"></P> -->
-  <button class="btn btn-primary" @click="sure">Submit</button>
-  <button class="btn btn-warning" @click="showMain``=true, showData=false">Back</button>
+<div id="showData" v-if="showData">
+  <label for="nameInfo">Your First Name: &nbsp;</label>
+  <span id="nameInfo" class="text-muted">{{fname}}</span><br>
+   <label for="nameInfo">Your Last Name: &nbsp;</label>
+  <span id="nameInfo" class="text-muted">{{lname}}</span><br>
+   <label for="nameInfo">Your Email: &nbsp;</label>
+  <span id="nameInfo" class="text-muted">{{email}}</span><br>
+   <label for="nameInfo">Your Age: &nbsp;</label>
+  <span id="nameInfo" class="text-muted">{{age}}</span><br>
+  <div class="d-flex justify-content-around">
+  <button class="btn btn-primary alig m-3" @click="sure">Submit</button>
+  <button class="btn btn-warning m-3" @click="showMain=true, showData=false">Back</button>
+  </div>
 </div>
 <div id="submmited" v-if="submited">
   <h3 class="text-center text-primary">Thank you your data submitted successfully</h3>
@@ -66,14 +79,22 @@ export default {
       fname:"",
       lname:"",
       email:"",
-      age:null,
+      age:"",
       showMain:true,
       showData:false,
       submited:false,
-      myTimeout:null
+      myTimeout:null,
+      errors: []
     } 
   },
   methods:{
+    checkForm : function(e){
+      if (isNan(this.age)) {     
+        this.errors = [];
+        this.errors.push('Age required as a number');
+        e.preventDefault();
+      }
+    },
     backToMain(){
       this.showMain= true;
       this.submited = false;
@@ -88,7 +109,7 @@ export default {
         this.lname = null,
         this.email = null,
         this.age = null,
-        this.myTimeout = setTimeout(this.backToMain, 5000);
+        this.myTimeout = setTimeout(this.backToMain, 4000);
       }
     },
   },
@@ -100,6 +121,10 @@ export default {
   template{
     height: 77vh;
   }
+  #showData{
+     margin-top: 3rem;
+    height: 70vh;
+  }
   img.animated-gif{
   margin: 1px auto;
   width: 200px;
@@ -108,5 +133,11 @@ export default {
   #submmited{
     margin-top: 3rem;
     height: 70vh;
+  }
+  form{
+    height: 77vh;
+  }
+  .form.div{
+    margin-top:3rem;
   }
 </style>
